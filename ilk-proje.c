8735147c06a91969be617dde3fbde0be76d57e82
ellyykorkmaz <elifcodes123@gmail.com>
2026-03-14 23:15:43 +0300

    derleyici hataları düzeltildi. ilk ayarlar.

---
+++ ilk-proje.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("ogrenci1\n");
+    printf("----------------\n\n");
+    printf("isim: elif\n");
+    printf("yas:21\n");
+    printf("bolum:biyomedical muhendisligi\n");
+    printf("------------------\n\n");
+    printf("ogrenci2\n");
+    // Buraya ogrenci2 sdffsdfsdf ekleyebilirsiniz
+    return 0;
+}
